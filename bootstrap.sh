9d455aa4a3c5348954d72f68dfdade19f46db334
Stan Manilov <stanislav.manilov@gmail.com>
2021-11-02 22:59:10 +0200

    Add keygen and repo cloning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-# assuming the system is Debian and git is installed
+# assuming the system is Debian
 sudo apt-get update
 sudo apt-get install vim -y
 sudo apt-get install tmux -y
 
-# assuming script is executed from the root of a clone of the bootstrap repo
+# assuming git is installed, pull config files and copy them to home directory
+# Generate a private/public key pair and print the public key
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
+echo "Here is the generated public key: add it to your github account"
+cat ~/.ssh/id_ed25519.pub
+read "Press [Enter] to continue..."
+
+# Checkout bootstrap repo in a temp folder
+tmpdir=$(mktemp -d)
+pushd $tmpdir
+git clone git@github.com:smanilov/bootstrap.git
+popd
+
 # copy config files to home
-cp -R home/* ~
+cp -R $tmpdir/home/* ~
+rm -rf $tmpdir
 
 # install vim plugins
 vim +PluginInstall +qall
