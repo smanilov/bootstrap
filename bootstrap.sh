cb7360c00e3dec499dec57f38a13097a2282f852
Stan Manilov <stanislav.manilov@gmail.com>
2021-11-02 23:07:38 +0200

    Fix read and add log messages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # assuming the system is Debian
+printf "${GREEN}Installing vim, tmux, and git${NC}"
 sudo apt-get update
 sudo apt-get install vim -y
 sudo apt-get install tmux -y
+sudo apt-get install git -y
 
-# assuming git is installed, pull config files and copy them to home directory
+printf "${GREEN}Pulling config files and copying them to home directory${NC}"
 # Generate a private/public key pair and print the public key
 ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
-echo "Here is the generated public key: add it to your github account"
+printf "${YELLOW}Here is the generated public key: add it to your github account${NC}"
 cat ~/.ssh/id_ed25519.pub
-read "Press [Enter] to continue..."
+echo "${YELLOW}... then, press [Enter] to continue...${NC}"
+read
 
 # Checkout bootstrap repo in a temp folder
 tmpdir=$(mktemp -d)
@@ -24,7 +32,7 @@ rm -rf $tmpdir
 # install vim plugins
 vim +PluginInstall +qall
 
-# install docker
+printf "${GREEN}Installing docker${NC}"
 sudo apt-get install \
     ca-certificates \
     curl \
@@ -41,7 +49,7 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-# install docker compose
+printf "${GREEN}Installing docker-compose${NC}"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
