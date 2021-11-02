87f2382db537fd2a4f225b60e545c0872e9acf6b
Stan Manilov <stanislav.manilov@gmail.com>
2021-11-02 23:36:55 +0200

    Add cherry on top

--- bootstrap.sh
+++ bootstrap.sh
@@ -58,3 +58,10 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+printf "${GREEN}Removing self${NC}\n"
+rm bootstrap.sh
+
+printf "${GREEN}Starting tmux${NC}\n"
+tmux
